9fb12ebbb3e4263b0e776891474d5e4f355450e2
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-06-29 07:17:50 +0200

    Add project files.

---
+++ ForLoops/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://www.udemy.com/course/csharp-tutorial-for-beginners/learn/lecture/2169340#content
+namespace ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i%2 == 0) //remainder of a division i by 2
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            for (int i = 10; i >= 1; i--)
+            {
+                if (i%2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
